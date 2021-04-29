0a136a3fb73bfce3a16eff4d663350c99803e0a8
lodevweb <lodevweb@outlook.com>
2021-04-29 14:33:05 +0200

    init

---
+++ TpHeritage/Cercle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpHeritage
+{
+    public class Cercle : Forme
+    {
+        public int Rayon { get; set; }
+    }
+}
---
+++ TpHeritage/Rectangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpHeritage
+{
+    public class Rectangle : Forme
+    {
+        public virtual int Largeur { get; set; }
+        public int Longueur { get; set; }
+       
+        public override double Aire => Largeur + Longueur;
+
+    }
+}
